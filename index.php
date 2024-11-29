bc66228bc4f2966ea7c169a1ce0186f66d356e02
Piyush Kumar Yadav <piyushkumar3@amityonline.com>
2024-11-30 04:44:53 +0530

    footer modification

--- index.php
+++ index.php
@@ -106,10 +106,34 @@
     </table>
   </div>
      <!-- FOOTER -->
-      <footer style="background:; height:120%;">
-        <div><p class="pull-right"><a href="#">Back to top</a></p>
-        <p>  2024 © All Rights Reserved developed by  <a href="https://github.com/Piyush73-dba">Piyush Kumar Yadav </a></p></div>
-      </footer>
+      <!-- Remove the container if you want to extend the Footer to full width. -->
+<div class="container my-5">
+
+<footer class="text-center text-lg-start" style="background-color: #db6930;">
+  <div class="container d-flex justify-content-center py-5">
+    <button type="button" class="btn btn-primary btn-lg btn-floating mx-2" style="background-color: #54456b;">
+      <i class="fab fa-facebook-f"></i>
+    </button>
+    <button type="button" class="btn btn-primary btn-lg btn-floating mx-2" style="background-color: #54456b;">
+      <i class="fab fa-youtube"></i>
+    </button>
+    <button type="button" class="btn btn-primary btn-lg btn-floating mx-2" style="background-color: #54456b;">
+      <i class="fab fa-instagram"></i>
+    </button>
+    <button type="button" class="btn btn-primary btn-lg btn-floating mx-2" style="background-color: #54456b;">
+      <i class="fab fa-twitter"></i>
+    </button>
+  </div>
+
+  <!-- Copyright -->
+  <div class="text-center text-white p-3" style="background-color: rgba(0, 0, 0, 0.2);">
+    © 2020 Copyright:
+    <a class="text-white" href="https://mdbootstrap.com/">MDBootstrap.com</a>
+  </div>
+  <!-- Copyright -->
+</footer>
+
+</div>
 
     </div><!-- /.container -->
 
