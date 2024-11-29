9626e8cae331ede54837a37652f6b7287c13e95b
Piyush Kumar Yadav <piyushkumar3@amityonline.com>
2024-11-30 04:24:04 +0530

    footer updated

--- index.php
+++ index.php
@@ -108,7 +108,7 @@
      <!-- FOOTER -->
       <footer style="background:; height:120%;">
         <p class="pull-right"><a href="#">Back to top</a></p>
-        <p>&copy;       2017 ProjectWorlds, Inc. &middot;  developed by  <a href="https://facebook.com/yugesh.verma.35">Piyush Kumar Yadav </a><a href="http://projectworlds.in">Privacy</a> &middot; <a href="http://projectworlds.in">Terms</a></p>
+        <p>&copy;    2024  &middot;  developed by  <a href="https://github.com/Piyush73-dba">Piyush Kumar Yadav </a></p>
       </footer>
 
     </div><!-- /.container -->
