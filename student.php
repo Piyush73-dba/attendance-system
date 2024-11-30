5903b003d2b7b14fb49ac596cd7a9cdad12d77b3
Piyush Kumar Yadav <piyushkumar3@amityonline.com>
2024-11-30 13:21:23 +0530

    footer 4

--- student.php
+++ student.php
@@ -66,66 +66,10 @@
   </form>
   </div>
   </div><!-- /.container -->
-   <!-<!-- Remove the container if you want to extend the Footer to full width. -->
-<div class                                                    = "container my-5">
-
-  <footer class                                               = "bg-dark text-center text-white">
-  <!-- Grid container -->
-  <div class                                                  = "container p-4 pb-0">
-    <!-- Section                                              : Form -->
-    <section class                                            = "">
-      <form action                                            = "">
-        <!--Grid row-->
-        <div class                                            = "row d-flex justify-content-center">
-          <!--Grid column-->
-          <div class                                          = "col-auto">
-            <p class                                          = "pt-2">
-              <strong>Sign up for our newsletter</strong>
-            </p>
-          </div>
-          <!--Grid column-->
-
-          <!--Grid column-->
-          <div class                                          = "col-md-5 col-12">
-            <!-- Email input -->
-            <div class                                        = "form-outline form-white mb-4">
-              <input type                                     = "email" id="form5Example2" class="form-control" />
-              <label class                                    = "form-label" for="form5Example2">Email address</label>
-            </div>
-          </div>
-          <!--Grid column-->
-
-          <!--Grid column-->
-          <div class                                          = "col-auto">
-            <!-- Submit button -->
-            <button type                                      = "submit" class="btn btn-outline-light mb-4">
-              Subscribe
-            </button>
-          </div>
-          <!--Grid column-->
-        </div>
-        <!--Grid row-->
-      </form>
-    </section>
-    <!-- Section                                              : Form -->
-  </div>
-  <!-- Grid container -->
-
-  <!-- Copyright -->
-  <div class                                                  = "text-center p-3" style="background-color: rgba(0, 0, 0, 0.2);">
-    © 2020 Copyright                                          : 
-    <a class                                                  = "text-white" href="https://mdbootstrap.com/">MDBootstrap.com</a>
-  </div>
-  <!-- Copyright -->
-</footer>
-  
-</div>
-<!-- End of .container -->
-      <footer style                                           = "background:; height:;">
-        <p class                                              = "pull-right"><a href="#">Back to top</a></p>
-        <p>2024 © All Rights Reserved developed by  <a href   = "https://github.com/Piyush73-dba">Piyush Kumar Yadav </a></p>
-      </footer>
-
-    
+   <div>
+    <footer>
+     <p>© 2024 Copyright: All Right Reserved</p><a class="text-white" href="https://github.com/Piyush73-dba">Developed by Piyush Yadav</a>
+    </footer>
+   </div>  
  </body>
 </html>
